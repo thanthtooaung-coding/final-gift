58971727eab540fbefa6fa65f972411873e25aa5
thanthtooaung <thanthtoo1285@gmail.com>
2025-10-26 19:03:14 +0700

    :sparkles: feat: added db url and frontend url

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://shop.pyaepyae.com", "https://shop.pyaepyae.com/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000", "https://shop.pyaepyae.com", "https://shop.pyaepyae.com/", "https://pyae-pyae-shop.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
